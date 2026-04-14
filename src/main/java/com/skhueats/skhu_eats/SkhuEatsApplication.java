23b2eb418d98f55d845e73ce0ce657f41bc7af87
Kim, MINA <kimmina020531@gmail.com>
2026-04-14 23:43:34 +0900

    init: 프로젝트 초기 세팅

---
+++ src/main/java/com/skhueats/skhu_eats/SkhuEatsApplication.java
@@ -0,0 +1,13 @@
+package com.skhueats.skhu_eats;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SkhuEatsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SkhuEatsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/skhueats/skhu_eats/SkhuEatsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.skhueats.skhu_eats;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SkhuEatsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
