8e494755bf50ceb72ddc53a0e7c2b033d4887b63
Kim, Hanmin <khm020601@naver.com>
2026-05-11 14:23:10 +0900

    chore: 불필요한 import 제거

--- src/main/java/com/skhueats/SkhuEatsApplication.java
+++ src/main/java/com/skhueats/SkhuEatsApplication.java
@@ -4,10 +4,9 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+
 public class SkhuEatsApplication {
-
-	public static void main(String[] args) {
-		SpringApplication.run(SkhuEatsApplication.class, args);
-	}
-
+    public static void main(String[] args) {
+        SpringApplication.run(SkhuEatsApplication.class, args);
+    }
 }
