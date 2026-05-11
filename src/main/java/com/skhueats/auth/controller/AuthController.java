8a9cbb4aa3b5fe56dd0aee89e07ddf6d6b45a944
Kim, Hanmin <khm020601@naver.com>
2026-05-11 13:56:16 +0900

    feat: 로그인, Refresh Token 재발급, 로그아웃 API 구현 (#4)

--- src/main/java/com/skhueats/auth/controller/AuthController.java
+++ src/main/java/com/skhueats/auth/controller/AuthController.java
@@ -1,17 +1,22 @@
 package com.skhueats.auth.controller;
 
 import com.skhueats.auth.dto.request.EmailRequest;
+import com.skhueats.auth.dto.request.LoginRequest;
+import com.skhueats.auth.dto.request.LogoutRequest;
+import com.skhueats.auth.dto.request.RegisterRequestDto;
+import com.skhueats.auth.dto.request.TokenRefreshRequest;
 import com.skhueats.auth.dto.request.VerifyCodeRequest;
+import com.skhueats.auth.dto.response.LoginResponse;
 import com.skhueats.auth.dto.response.RegisterResponseDto;
 import com.skhueats.auth.service.AuthService;
-import com.skhueats.auth.dto.request.RegisterRequestDto;
+import jakarta.validation.Valid;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import jakarta.validation.Valid;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
 import java.util.Map;
 
 @RestController
@@ -39,10 +44,26 @@ public class AuthController {
     @PostMapping("/register")
     public ResponseEntity<RegisterResponseDto> register(@Valid @RequestBody RegisterRequestDto request) {
         RegisterResponseDto response = authService.register(request);
-
         return ResponseEntity
                 .status(HttpStatus.CREATED)
                 .body(response);
     }
-}
 
+    @PostMapping("/login")
+    public ResponseEntity<LoginResponse> login(@Valid @RequestBody LoginRequest request) {
+        LoginResponse response = authService.login(request);
+        return ResponseEntity.ok(response);
+    }
+
+    @PostMapping("/refresh")
+    public ResponseEntity<LoginResponse> refresh(@Valid @RequestBody TokenRefreshRequest request) {
+        LoginResponse response = authService.refresh(request);
+        return ResponseEntity.ok(response);
+    }
+
+    @PostMapping("/logout")
+    public ResponseEntity<Map<String, String>> logout(@Valid @RequestBody LogoutRequest request) {
+        authService.logout(request);
+        return ResponseEntity.ok(Map.of("message", "로그아웃 완료"));
+    }
+}
