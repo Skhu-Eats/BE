56b90fd80352581ae88d85a56531c76bb5a629e0
Kim, MINA <kimmina020531@gmail.com>
2026-05-25 01:18:34 +0900

    [#6] feat: Spring Security 접근 권한 설정

--- src/main/java/com/skhueats/global/config/SecurityConfig.java
+++ src/main/java/com/skhueats/global/config/SecurityConfig.java
@@ -9,8 +9,6 @@ import org.springframework.http.HttpStatus;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.config.http.SessionCreationPolicy;
-import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
-import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.HttpStatusEntryPoint;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
@@ -43,9 +41,9 @@ public class SecurityConfig {
                                 "/error",
                                 "/swagger-ui/**",
                                 "/v3/api-docs/**",
-                                "/auth/check-nickname",
-                                "/error"
+                                "/auth/check-nickname"
                         ).permitAll()
+                        .requestMatchers(HttpMethod.GET, "/users/me").authenticated()
                         .anyRequest().authenticated()
                 )
                 .httpBasic(AbstractHttpConfigurer::disable)
