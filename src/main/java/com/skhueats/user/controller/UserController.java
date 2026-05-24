e8a2866799cce5d252eb26659b9ea86ba6de84d1
Kim, MINA <kimmina020531@gmail.com>
2026-05-25 01:12:49 +0900

    [#6] feat: 내 프로필 조회 API Controller 구현

--- src/main/java/com/skhueats/user/controller/UserController.java
+++ src/main/java/com/skhueats/user/controller/UserController.java
@@ -1,19 +1,26 @@
 package com.skhueats.user.controller;
 
-import com.skhueats.auth.dto.request.RegisterRequestDto;
+import com.skhueats.user.dto.response.MyProfileResponseDto;
 import com.skhueats.user.service.UserService;
-import jakarta.validation.Valid;
-import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping("/auth")
+@RequestMapping("/users")
 public class UserController {
 
     private final UserService userService;
 
     public UserController(UserService userService) {
         this.userService = userService;
+    }
+
+    @GetMapping("/me")
+    public ResponseEntity<MyProfileResponseDto> getMyProfile() {
+        MyProfileResponseDto response = userService.getMyProfile();
+
+        return ResponseEntity.ok(response);
     }
 }
