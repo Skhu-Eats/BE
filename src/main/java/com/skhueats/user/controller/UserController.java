15a2a274cf8d60f484606b538db2dd070f61e585
Kim, Hanmin <khm020601@naver.com>
2026-05-26 09:36:42 +0900

    [#9] feat: 회원 탈퇴 컨트롤러 구현

--- src/main/java/com/skhueats/user/controller/UserController.java
+++ src/main/java/com/skhueats/user/controller/UserController.java
@@ -1,19 +1,24 @@
 package com.skhueats.user.controller;
 
-import com.skhueats.auth.dto.request.RegisterRequestDto;
+import com.skhueats.user.CustomUserDetails;
 import com.skhueats.user.service.UserService;
-import jakarta.validation.Valid;
-import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
-@RequestMapping("/auth")
+@RequestMapping("/users")
 public class UserController {
 
     private final UserService userService;
 
     public UserController(UserService userService) {
         this.userService = userService;
+    }
+
+    @DeleteMapping("/me")
+    public ResponseEntity<Void> deleteAccount(@AuthenticationPrincipal CustomUserDetails userDetails) {
+        userService.deleteAccount(userDetails.getUsername());
+        return ResponseEntity.noContent().build();
     }
 }
