9e4e9a10a85bb64b7569972cd2459beef4c31b69
Kim, MINA <kimmina020531@gmail.com>
2026-06-01 03:35:32 +0900

    [#7] feat: 내 프로필 수정 Controller 구현

--- src/main/java/com/skhueats/user/controller/UserController.java
+++ src/main/java/com/skhueats/user/controller/UserController.java
@@ -1,26 +1,35 @@
 package com.skhueats.user.controller;
 
+import com.skhueats.user.dto.request.UpdateMyProfileRequestDto;
 import com.skhueats.user.dto.response.MyProfileResponseDto;
 import com.skhueats.user.service.UserService;
+import jakarta.validation.Valid;
+import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PatchMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/users")
+@RequiredArgsConstructor
 public class UserController {
 
     private final UserService userService;
 
-    public UserController(UserService userService) {
-        this.userService = userService;
-    }
-
     @GetMapping("/me")
     public ResponseEntity<MyProfileResponseDto> getMyProfile() {
-        MyProfileResponseDto response = userService.getMyProfile();
+        MyProfileResponseDto responseDto = userService.getMyProfile();
+        return ResponseEntity.ok(responseDto);
+    }
 
-        return ResponseEntity.ok(response);
+    @PatchMapping("/me")
+    public ResponseEntity<MyProfileResponseDto> updateMyProfile(
+            @Valid @RequestBody UpdateMyProfileRequestDto requestDto
+    ) {
+        MyProfileResponseDto responseDto = userService.updateMyProfile(requestDto);
+        return ResponseEntity.ok(responseDto);
     }
 }
